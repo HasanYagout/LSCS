1f68a50b563df7cd916468e43da0a9737b187303
dev4 <yagouthasan3@gmail.com>
2024-05-14 19:19:10 +0300

    merge yosifYag into master

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
+    plugins: [ 
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
