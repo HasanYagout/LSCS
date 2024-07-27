d5a6dfd7e0135a702c4790fda66ecf12541a4945
Hasan Yagout <yagouthasan@gmail.com>
2024-07-27 18:19:08 +0300

    fix

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AlumniController;
-
 use App\Http\Controllers\Admin\CompanyController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventCategoryController;
@@ -11,15 +10,12 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\JobsController;
 use App\Http\Controllers\Admin\NewsCategoryController;
 use App\Http\Controllers\Admin\NewsController;
-use App\Http\Controllers\Admin\NewsTagController;
 use App\Http\Controllers\Admin\NoticeCategoryController;
 use App\Http\Controllers\Admin\NoticeController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\StoryController;
 use App\Http\Controllers\Admin\StudentController;
-
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])->group(function () {
