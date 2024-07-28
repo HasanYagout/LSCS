824445b554fc1c4365bbb4a44f77a2bf8f9521a1
Hasan Yagout <yagouthasan@gmail.com>
2024-07-28 16:00:27 +0300

    fix

--- resources/views/admin/jobs/job_post_view.blade.php
+++ resources/views/admin/jobs/job_post_view.blade.php
@@ -8,6 +8,7 @@
     <!-- Page content area start -->
     <div class="p-30" >
         <div class="">
+            <input type="hidden" id="job-post-list-route" value="{{ route('admin.jobs.applied',['id'=>$jobPostData->id]) }}">
             <h4 class="fs-24 fw-500 lh-34 text-black pb-16">{{ __('Post Details') }}</h4>
             <div class="bg-white bd-half bd-c-ebedf0 bd-ra-25 p-30">
                 <div class="d-flex flex-column rg-22 max-w-890 pb-30">
@@ -44,12 +45,78 @@
 
                 </div>
 
+                <div id="search-section">
+                    <div class="collapse" id="collapseExample">
+                        <div class="alumniFilter">
+                            <h4 class="fs-18 fw-500 lh-38 text-1b1c17 pb-10">{{__('Filter your search')}}</h4>
+                            <div class="filterOptions">
+                                <div class="item">
+                                    <div class="primary-form-group">
+                                        <div class="primary-form-group-wrap">
+                                            <label for="Department" class="form-label">{{__('Department')}}</label>
 
+                                            <select class="sf-select-without-search primary-form-control" name='department'
+                                                    id='department'>
+                                                <option selected="" value=0>{{__('All Majors')}}</option>
+                                                @foreach ($majors as $major)
+                                                    <option value="{{ $major->name}}">{{ $major->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <div class="primary-form-group">
+                                        <div class="primary-form-group-wrap">
+                                            <label for="passing_year" class="form-label">{{__('Passing Year')}}</label>
+                                            <select class="sf-select-without-search primary-form-control" name='passing_year'
+                                                    id='passing-year'>
+                                                <option selected="" value=0>{{__('All Year')}}</option>
+                                                @foreach ($years as $year)
+                                                    <option value="{{ $year }}">{{ $year}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <div class="primary-form-group">
+                                        <div class="primary-form-group-wrap">
+                                            <label for="is_member" class="form-label">{{__('GPA')}}</label>
+                                            <select class="sf-select-without-search primary-form-control" name='gpa'
+                                                    id='GPA'>
+                                                <option value="-1" selected>{{__('All')}}</option>
+                                                @foreach ($gpas as $gpa)
+                                                    <option value="{{ $gpa }}">{{ $gpa }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button
+                                    class="bg-cdef84 border-0 bd-ra-12 py-13 px-26 fs-15 fw-500 lh-25 text-black hover-bg-one advance-filter">{{__('Search Now')}}</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <table class="table zTable" id="appliedJobsTable">
+                    <thead>
+                    <tr>
+                        <th scope="col"><div>{{ __('Name') }}</div></th>
+                        <th scope="col"><div>{{ __('GPA') }}</div></th>
+                        <th scope="col"><div>{{ __('Major') }}</div></th>
+                        <th scope="col"><div>{{ __('Graduation Year') }}</div></th>
+                        <th class="w-110 text-center" scope="col"><div>{{ __('Action') }}</div></th>
+                    </tr>
+                    </thead>
+                </table>
             </div>
         </div>
     </div>
 @endsection
 
 @push('script')
-    <script src="{{ asset('alumni/js/job_post.js') }}"></script>
+    <script src="{{ asset('public/admin/js/applied_jobs.js') }}"></script>
 @endpush
